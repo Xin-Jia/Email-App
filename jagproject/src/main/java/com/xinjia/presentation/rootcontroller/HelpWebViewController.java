4d35f9626cb39f081087946b4d6ec623e9982200
xin jia cao <xinjia.caoxin@gmail.com>
2020-11-24 17:03:46 -0500

    added about for french

--- jagproject/src/main/java/com/xinjia/presentation/rootcontroller/HelpWebViewController.java
+++ jagproject/src/main/java/com/xinjia/presentation/rootcontroller/HelpWebViewController.java
@@ -1,5 +1,6 @@
 package com.xinjia.presentation.rootcontroller;
 
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.web.WebView;
 import org.slf4j.Logger;
@@ -13,6 +14,9 @@ import org.slf4j.LoggerFactory;
  */
 public class HelpWebViewController {
 
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+    
     @FXML // fx:id="helpWebView"
     private WebView helpWebView; // Value injected by FXMLLoader
     private final static Logger LOG = LoggerFactory.getLogger(HelpWebViewController.class);
@@ -20,15 +24,22 @@ public class HelpWebViewController {
     /**
      * Called by the FXMLLoader when initialization is complete. When the FXML
      * is loaded, if a control is not present, an exception is thrown and quits
-     * the FXML loading process. Load the HTML file to be displayed.
+     * the FXML loading process. Load the HTML file to be displayed based on the locale.
      */
     @FXML
     void initialize() {
         LOG.info("Displaying WebView");
         assert helpWebView != null : "fx:id=\"helpWebView\" was not injected: check your FXML file 'HelpWebView.fxml'.";
+        String htmlFile = "";
+        if(resources.getString("help").equals("Help")){
+            htmlFile = "help.html";
+        }
+        else if(resources.getString("help").equals("Aide")){
+                        
+            htmlFile = "help_fr.html";
+        }
 
-        final String html = "help.html";
-        final java.net.URI uri = java.nio.file.Paths.get(html).toAbsolutePath().toUri();
+        final java.net.URI uri = java.nio.file.Paths.get(htmlFile).toAbsolutePath().toUri();
         //Load the given HTML file
         helpWebView.getEngine().load(uri.toString());
     }
